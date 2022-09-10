e0a0ebbcc96bc0e32dfc3ae1227856a23d0b8dd5
hritik <hchokker7@gmail.com>
2022-09-10 23:01:24 +0530

    added marketing deployment config

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -6,6 +6,7 @@ import App from "./App";
 const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
+
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === "development") {
