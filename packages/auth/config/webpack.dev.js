ffde7fd7bdffa36f8e067458362865f215e3e98e
hritik <hchokker7@gmail.com>
2022-09-12 17:31:59 +0530

    dashboard and auth added

--- packages/auth/config/webpack.dev.js
+++ packages/auth/config/webpack.dev.js
@@ -5,10 +5,10 @@ const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   output: {
-    publicPath: "http://localhost:8081/",
+    publicPath: "http://localhost:8082/",
   },
   devServer: {
-    port: 8081,
+    port: 8082,
     historyApiFallback: {
       index: "/index.html",
     },
@@ -16,10 +16,10 @@ const devConfig = {
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
+      name: "auth",
       filename: "remoteEntry.js",
       exposes: {
-        "./MarketingApp": "./src/bootstrap",
+        "./AuthApp": "./src/bootstrap",
       },
       // shared: ["react", "react-dom" ],
       shared: packageJson.dependencies,
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import {createBrowserHistory} from history;
+import Header from "./components/Header";
+import Progress from "./components/Progress";
+const MarketingApp = lazy(() => import("./components/MarketingApp"));
+const AuthApp = lazy(() => import("./components/AuthApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ma",
+});
 
+const history = createBrowserHistory()
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  useEffect(()=>{
+    if(isSignedIn){
+      history.push('/dashboard');
+    }
+  },[isSignedIn])
   return (
-    <div>
-      <h1>Container app !!!</h1>
-      <hr></hr>
-      <MarketingApp />
-    </div>
+    <Router history={history}>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthApp onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+              <Router path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardApp  />
+              </Router>
+              <Route path="/">
+                <MarketingApp />
+              </Route>
+            </Switch>
+          </Suspense>
+        </div>
+      </StylesProvider>
+    </Router>
   );
 };
-
